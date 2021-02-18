eacea264c6b0b19029589c51e2eda3624bbabc8a
Jacques de Chalendar <jdechalendar@stanford.edu>
2021-02-18 10:56:06 -0800

    added demo notebooks

--- src/gridemissions/__init__.py
+++ src/gridemissions/__init__.py
@@ -1,7 +1,7 @@
 from .configure import config
 
 
-api_base_prod = "http://ec2-54-200-161-28.us-west-2.compute.amazonaws.com"
+api_base_prod = "http://ec2-18-221-223-189.us-east-2.compute.amazonaws.com"
 api_base_dev = "http://localhost:5000"
 
 base_url = api_base_prod
