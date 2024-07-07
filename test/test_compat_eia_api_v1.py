33fe12fb15440c0ef310e0a3c92631d79cc8562b
Jacques de Chalendar <jdechalendar@stanford.edu>
2024-07-07 08:10:12 +0200

    updates to tests
    * `test_workflows`: updated float formats
    * `test_compat_eia_api_v1`: EmissionsCalc does not allow NaNs

--- test/test_compat_eia_api_v1.py
+++ test/test_compat_eia_api_v1.py
@@ -12,6 +12,7 @@ def test_compat():
     # Check we can still load data in the old format (EIA v1 API)
     # I still have some of those files lying around from old papers...
     elec = ge.read_csv(get_path("EBA_elec.csv"), api_module=eia_api_v1)
+    elec.df = elec.df.fillna(0.0)  # EmissionsCalc does not allow NaNs
     EF = emissions.EMISSIONS_FACTORS["CO2"].copy()
     EF["NG"] = EF["GAS"]
     del EF["GAS"]
