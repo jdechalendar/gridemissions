3bfc778df7022cb47d214499a8c13bb8d1f77970
Jacques de Chalendar <jdechalendar@stanford.edu>
2024-07-07 14:48:18 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="gridemissions",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    version="0.1.9.dev1",
+    version="0.1.10",
     python_requires=">=3.11",
     install_requires=["requests", "pandas>=2.1.0", "matplotlib>=3.4.2"],
     extras_require={
